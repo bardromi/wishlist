54bad0d81507addecb2814163ca30c19adda3e59
Bar <bardromi@gmail.com>
2019-09-28 23:37:31 +0300

    Added:

--- internal/mid/logger.go
+++ internal/mid/logger.go
@@ -14,10 +14,10 @@ func Chain(h http.HandlerFunc, middleware ...func(http.HandlerFunc) http.Handler
 }
 
 func Logger(next http.HandlerFunc) http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 		// Do stuff here
 		log.Println(r.Method, r.RequestURI)
 		// Call the next handler, which can be another middleware in the chain, or the final handler.
 		next.ServeHTTP(w, r)
-	})
+	}
 }
