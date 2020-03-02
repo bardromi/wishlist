4c76009e15d4849385763f68e8862f1562041ed2
Bar <bardromi@gmail.com>
2020-03-02 22:17:14 +0200

    Added:\n wish table to db script

--- internal/setup.sql
+++ internal/setup.sql
@@ -1,24 +1,30 @@
+drop table wishes;
 drop table users;
 
 create table users
 (
-    id         uuid primary key,
+    id         uuid,
     name       text      not null,
     email      text unique,
     password   text      not null,
-    created_at timestamp not null
+    created_at timestamp not null,
+
+    PRIMARY KEY (id)
 );
 
-create table wishes
+CREATE TABLE wishes
 (
-    id SERIAL  primary key,
-    owner_id uuid REFERENCES users(id),
+    id SERIAL  ,
+    owner_id uuid,
     title text,
     price numeric,
-    created_at timestamp not null
-)
+    created_at timestamp not null,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (owner_id) REFERENCES users(id) ON DELETE CASCADE
+);
 
 -- psql wishlist < C:\Users\Bar\go\src\github.com\bardromi\wishlist\internal\setup.sql
 
--- If using posershell
+-- If using powershell
 -- cmd /c 'psql wishlist < C:\Users\Bar\go\src\github.com\bardromi\wishlist\internal\setup.sql'
