19d18967773053f5a0c87aa810fe8eb95f30b816
Bar <bardromi@gmail.com>
2020-03-11 12:16:29 +0200

    FieldsThunk  used for fields lazy loading to prevent cyrcle issues

--- internal/gql/types.go
+++ internal/gql/types.go
@@ -4,42 +4,54 @@ import (
 	"github.com/graphql-go/graphql"
 )
 
-var userType = graphql.NewObject(
-	graphql.ObjectConfig{
-		Name: "User",
-		Fields: graphql.Fields{
-			"id": &graphql.Field{
-				Type: graphql.String,
-			},
-			"name": &graphql.Field{
-				Type: graphql.String,
-			},
-			"email": &graphql.Field{
-				Type: graphql.String,
-			},
+func userType(typeResolver TypeResolver) *graphql.Object {
+	var userType = graphql.NewObject(
+		graphql.ObjectConfig{
+			Name: "User",
+			Fields: graphql.FieldsThunk(func() graphql.Fields {
+				return graphql.Fields{
+					"id": &graphql.Field{
+						Type: graphql.String,
+					},
+					"name": &graphql.Field{
+						Type: graphql.String,
+					},
+					"email": &graphql.Field{
+						Type: graphql.String,
+					},
+					"wishes": &graphql.Field{
+						Type:    graphql.NewList(wishType(typeResolver)),
+						Resolve: typeResolver.getUserWishes,
+					},
+				}
+			}),
 		},
-	},
-)
+	)
+
+	return userType
+}
 
 func wishType(typeResolver TypeResolver) *graphql.Object {
 	var wishType = graphql.NewObject(
 		graphql.ObjectConfig{
 			Name: "Wish",
-			Fields: graphql.Fields{
-				"id": &graphql.Field{
-					Type: graphql.String,
-				},
-				"owner": &graphql.Field{
-					Type:    userType,
-					Resolve: typeResolver.getUserbyID,
-				},
-				"title": &graphql.Field{
-					Type: graphql.String,
-				},
-				"price": &graphql.Field{
-					Type: graphql.Float,
-				},
-			},
+			Fields: graphql.FieldsThunk(func() graphql.Fields {
+				return graphql.Fields{
+					"id": &graphql.Field{
+						Type: graphql.String,
+					},
+					"owner": &graphql.Field{
+						Type:    userType(typeResolver),
+						Resolve: typeResolver.getUserByID,
+					},
+					"title": &graphql.Field{
+						Type: graphql.String,
+					},
+					"price": &graphql.Field{
+						Type: graphql.Float,
+					},
+				}
+			}),
 		},
 	)
 	return wishType
